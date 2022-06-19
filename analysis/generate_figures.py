bdb48ebbb49be163c2f6ea683b11cbc8655e1104
Stan van Lier <vlierdata@gmail.com>
2022-06-19 23:07:20 +0200

    consitent categorical order

--- analysis/generate_figures.py
+++ analysis/generate_figures.py
@@ -55,10 +55,11 @@ plot_for_datasets = ['SVHN', 'MNIST']
 logy = False
 for ci, (col, nicename) in enumerate(histplot_for_params):
     fig, ax_ds = plt.subplots(1, len(plot_for_datasets), figsize=(7,3))
+    subr = results[results.extra__dataset==ds].sort_values(col)
     for di, ds in enumerate(plot_for_datasets):
         ax = ax_ds[di]
         islastplot = di==len(plot_for_datasets)-1
-        sns.histplot(ax=ax, data=results[results.extra__dataset==ds], x=col,
+        sns.histplot(ax=ax, data=subr, x=col,
                     hue='N Classes', multiple='stack', log_scale=(False, logy),
                     legend=islastplot,
                     )
@@ -80,10 +81,11 @@ plot_for_algos = ['Genetic', 'Random']
 logy = False
 for ci, (col, nicename) in enumerate(histplot_for_params):
     fig, ax_ds = plt.subplots(1, len(plot_for_algos), figsize=(7,3))
+    subr = results[results['Search Method']==ds].sort_values(col)
     for di, ds in enumerate(plot_for_algos):
         ax = ax_ds[di]
         islastplot = di==len(plot_for_algos)-1
-        sns.histplot(ax=ax, data=results[results['Search Method']==ds], x=col,
+        sns.histplot(ax=ax, data=subr, x=col,
                     hue='N Classes', multiple='stack', log_scale=(False, logy),
                     legend=islastplot,
                     )
